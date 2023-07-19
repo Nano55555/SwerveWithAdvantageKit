cc1617294d25036bb88d70e7aa060bbf1d1587b7
Nano55555 <nguyen.n@prhsrobotics.com>
2023-07-19 19:53:23 -0400

    k

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "SwerveWithAdvantageKit";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "5bed9d3e5ccb61df1814307112537baca4621d23";
+  public static final String GIT_DATE = "2023-07-18 19:20:21 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2023-07-19 19:52:19 EDT";
+  public static final long BUILD_UNIX_TIME = 1689810739597L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
